f7295b9c6f1cafe555839f15de2ecb09d4d4558e
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-06-21 20:57:06 +0500

    Add data to one to few relationship for the users model

--- users.js
+++ users.js
@@ -44,5 +44,57 @@ main()
 
     // User Model
 
-
     const User = mongoose.model("User", userSchema);
+
+    // Adding Data to the user model
+
+
+    const addUsers = async() =>{
+
+        const user1 = new User({
+
+            username : "Shoaib Akhtar ",
+
+            addresses: [
+
+                {   
+                    _id: false,
+                    location : "Badarabad Colony near suigas office",
+                    city: "karachi"
+
+                }
+            ],
+
+
+
+        })
+
+        user1.addresses.push({
+            location : "Defence Hosuing Society",
+            city: "Karachi"
+
+        })
+
+
+        let result = await user1.save()
+
+        console.log(result);
+
+
+
+    }
+
+
+    addUsers()
+        
+        .then((res)=>{
+
+            console.log("DataAdded Sucessfully");
+
+        }) 
+
+        .catch((err)=>{
+
+            console.log("Error Occured", err);
+
+        })
